33e75043d072acf9862bc1b5a00cf638ca1ccb10
Gautam Dhiman <gautamdhiman8708311909@gmail.com>
2026-04-26 15:05:58 +0530

    fixed photos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,7 +18,7 @@ const items: Item[] = [
     title: 'Waste Management System',
     description: 'Real-time AI analytics dashboard with live visualization.',
     tags: ['React', 'Node.js','Tailwind CSS'],
-    image: 'src/assets/Management-Waste.jpg',
+    image: 'src/assets/management.svg',
     url: 'https://management-waste.vercel.app/',
     type: 'Project',
     category: 'Web App',
